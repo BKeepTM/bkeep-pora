b0afc789dd477a0f7b0f067ec052652800678d27
JernejL123 <jernej.lobnik@gmail.com>
2026-01-08 12:41:23 +0100

    BKP-10 HiveRecyclerView in Adapter

--- app/src/main/java/com/example/bkeep/network/api/HiveApi.kt
+++ app/src/main/java/com/example/bkeep/network/api/HiveApi.kt
@@ -1,5 +1,6 @@
 package com.example.bkeep.network.api
 
+import com.example.lib.data.hive.Hive
 import com.example.lib.data.location.HiveLocation
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,4 +9,7 @@ interface HiveApi {
 
     @GET("location/list")
     suspend fun getHiveLocations(): Response<List<HiveLocation>>
+
+    @GET("/hive/list")
+    suspend fun getHiveByUserId(): Response<List<Hive>>
 }
